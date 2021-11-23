dfd88085844c58d003b7fa987e533edae8d0281d
dineshk876 <dineshk876@gmail.com>
2021-11-23 19:15:13 +0530

    frame1

---
+++ src/test/java/org/test/Child.java
@@ -0,0 +1,19 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.sample.Base;
+
+public class Child extends Base{
+public static void main(String[] args) throws Throwable {
+	launch();
+	url("https://www.facebook.com/");
+	maximize();
+	waits();
+	WebElement search = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+	tofill(search, excelRead("Sheet1", 1, 5));
+	
+	WebElement click = driver.findElement(By.xpath("//*[@id=\"pass\"]"));
+	tofill(click, excelRead("Sheet1", 1, 6));
+} 
+}
