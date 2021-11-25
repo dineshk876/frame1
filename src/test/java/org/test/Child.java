a85eb37ac1ab5d915610fe04e65f0f23dd7436bf
dineshk876 <dineshk876@gmail.com>
2021-11-25 20:17:05 +0530

    kumar is updated

--- src/test/java/org/test/Child.java
+++ src/test/java/org/test/Child.java
@@ -11,11 +11,14 @@ public static void main(String[] args) throws Throwable {
 	maximize();
 	waits();
 	WebElement search = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-	tofill(search, excelRead("Sheet1", 2, 5));
+	tofill(search, excelRead("Sheet1", 1, 5));
 	
 	WebElement click = driver.findElement(By.xpath("//*[@id=\"pass\"]"));
-	tofill(click, excelRead("Sheet1", 2, 6));
+	tofill(click, excelRead("Sheet1", 1, 6));
 	
 	driver.findElement(By.xpath("//*[@id=\"u_0_d_pE\"]")).click();
+	
+	driver.getCurrentUrl();
+	driver.getTitle();
 } 
 }
